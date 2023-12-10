8aca362f09cc5c6859603a804897d181d779ac2d
yonatan-nyo <yonatan.njoto@gmail.com>
2023-12-10 19:27:37 +0700

    created react app with nodejs server

---
+++ websocket/server/app.js
@@ -0,0 +1,11 @@
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    console.log("received: %s", data);
+  });
+
+  ws.send("something");
+});
